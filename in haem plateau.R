141858fbf254ac3924866d3e0513561fa2a6ca10
unknown <emckenzie@zoology.ubc.ca>
2020-11-29 20:56:06 -0800

    gorking ggplot of means/ sds and manual colour

---
+++ in haem plateau.R
@@ -0,0 +1,138 @@
+install.packages("tidyverse")
+library(tidyverse)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(Hmisc)
+install.packages("Hmisc")
+
+rm(DMSO_timecourse)
+head(DMSO_timecourse)
+
+is.data.frame(DMSO_timecourse)
+
+#make single time variable
+
+DMSOlong <-
+  pivot_longer(
+    DMSO_timecourse,
+    cols = c(`pre.inj`, `1h`, `2h`, `3h`, `4h`, `5h`),
+    names_to = "h", values_to = "area"
+  ) %>%
+  select(ant.post, larva, h, area) %>%
+  mutate_at("h", str_replace, "pre.inj", "0") %>% ## pre.inj to 0
+  mutate_at("h", str_replace, "1h", "1") %>%
+  mutate_at("h", str_replace, "2h", "2") %>%
+  mutate_at("h", str_replace, "3h", "3") %>%
+  mutate_at("h", str_replace, "4h", "4") %>%
+  mutate_at("h", str_replace, "5h", "5")
+
+DMSOlong$h <- as.numeric(DMSOlong$h)
+DMSOlong$h <- as_factor(DMSOlong$h)
+
+print(DMSOlong)
+
+#making % change column
+DMSOlong.pct <- DMSOlong %>%
+  group_by(ant.post, larva) %>%
+  mutate(
+    area.pct.change = ((area - area[1]) / area[1]
+    )*100) %>%
+  as_tibble(DMSOlong.pct) %>%
+  ungroup()
+
+print(DMSOlong.pct, n= 50)
+
+# makes separate anterior/ posterior dataframes
+anterior <- subset(DMSOlong.pct, ant.post == "ant", 
+                   select = c(larva, h, area, area.pct.change))
+
+posterior <- subset(DMSOlong.pct, ant.post == "post", 
+                    select = c(larva, h, area, area.pct.change))
+
+print(anterior, n=30)
+
+#making means
+ant.means <- anterior %>% 
+  group_by(h) %>% 
+  dplyr::summarise(
+    ant = mean(area.pct.change))
+
+print(ant.means, n= 36)
+
+post.means <- posterior %>% 
+  group_by(h) %>% 
+  summarise(
+    post = mean(area.pct.change))
+
+print(post.means, n= 36)
+
+#combining means
+means <- ant.means
+
+means$post <- post.means$post
+print(means, n= 36)
+
+#make sd values
+ant.stdv <- anterior %>%
+  group_by(h) %>%
+  dplyr::summarize(
+    ant = sd(area.pct.change))
+
+print(ant.stdv)
+
+post.stdv <- posterior %>%
+  group_by(h) %>%
+  dplyr::summarize(
+    post = sd(area.pct.change))
+
+print(post.stdv)
+
+#combine sd values
+stdv <- post.stdv
+stdv$ant <- ant.stdv$ant
+print(stdv)
+
+#combine means and sdtvs using left join after pivoting long
+mean.long <- means %>% 
+  pivot_longer(
+    cols = c(`ant`, `post`),
+    names_to = "antpost", values_to = "area")
+
+print(mean.long)
+
+stdv.long <- stdv %>%
+  pivot_longer(
+    cols = c(`ant`, `post`),
+    names_to = "antpost", values_to = "stdv")
+
+print(stdv.long)
+
+mean.sd <-
+  left_join(mean.long, stdv.long, by = c("h", "antpost"))
+
+print(mean.sd)
+
+######## plotting ########
+
+#plot means/ sd
+ggplot(data = mean.sd, aes(x= h)) +
+  geom_point(aes(y= area, colour= antpost)) +
+  geom_line(aes(x= h, y= area, group= antpost, colour= antpost)) +
+  geom_errorbar(aes(x= h, group= antpost, colour= antpost,
+                    ymin= area - stdv, 
+                    ymax= area + stdv), 
+                group= "antpost",
+                width= 0.5) +
+  scale_color_manual(values=c("#D55E00", "#009e73")) +
+  labs(x = "h", y = "% change") +
+  theme_classic()
+
+
+#plot the two dataframes together (raw data)
+ggplot(data = anterior, aes(y= area.pct.change , x= min, group= larva, colour= treatment)) +
+  geom_line() +
+  geom_line(data = posterior, linetype= "dashed") +
+  labs(x = "min", y = "% change") +
+  theme_classic() 
+
